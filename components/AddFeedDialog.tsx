141194d55f3c700ef9bccfc3c277d13dbe1e846d
Eric Fu <fuyufjh@gmail.com>
2022-05-22 13:32:49 +0800

    little refactor

--- components/AddFeedDialog.tsx
+++ components/AddFeedDialog.tsx
@@ -10,69 +10,52 @@ import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add'
 
-export default function AddFeedDialog() {
+interface Props {
+  open: boolean;
+  handleClose: () => void;
+}
 
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+export default function AddFeedDialog(props: Props) {
+  const { open, handleClose } = props;
 
   return (
-    <Box>
-      <Box>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Add RSS Feed</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              To subscribe to this website, please enter your email address here. We
-              will send updates occasionally.
-            </DialogContentText>
-            <TextField
-              margin="dense"
-              id="name"
-              label="Name"
-              type="text"
-              fullWidth
-              variant="standard"
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="url"
-              label="URL"
-              type="url"
-              fullWidth
-              variant="standard"
-            />
-            <TextField
-              margin="dense"
-              id="keywords"
-              label="Keywords (optional)"
-              type="text"
-              fullWidth
-              variant="standard"
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button variant='contained' onClick={handleClose}>Subscribe</Button>
-          </DialogActions>
-        </Dialog>
-      </Box>
-      <Box sx={{
-        position: 'fixed',
-        bottom: 20,
-        right: 20,
-      }}>
-        <Fab color="primary" aria-label="add" onClick={handleClickOpen}>
-          <AddIcon />
-        </Fab>
-      </Box>
-    </Box >
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Add RSS Feed</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To subscribe to this website, please enter your email address here. We
+          will send updates occasionally.
+        </DialogContentText>
+        <TextField
+          margin="dense"
+          id="name"
+          label="Name"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="url"
+          label="URL"
+          type="url"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          margin="dense"
+          id="keywords"
+          label="Keywords (optional)"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button variant='contained' onClick={handleClose}>Subscribe</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,19 @@ import Copyright from '../src/Copyright';
 import FeedList from '../components/FeedList'
 import AddFeedDialog from '../components/AddFeedDialog';
 import RssInfoCard from '../components/RssInfoCard'
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 const Home: NextPage = () => {
+  const [openAddDialog, setOpenAddDialog] = React.useState(false);
+
+  const onClickAddFeed = () => {
+    setOpenAddDialog(true);
+  }
+  const handleClose = () => {
+    setOpenAddDialog(false);
+  }
+
   return (
     <Box>
       <Container>
@@ -26,8 +37,17 @@ const Home: NextPage = () => {
         }}>
           <FeedList />
         </Box>
-        <AddFeedDialog />
+        <AddFeedDialog open={openAddDialog} handleClose={handleClose} />
       </Container>
+      <Box sx={{
+        position: 'fixed',
+        bottom: 20,
+        right: 20,
+      }}>
+        <Fab color="primary" aria-label="add" onClick={onClickAddFeed}>
+          <AddIcon />
+        </Fab>
+      </Box>
     </Box>
   );
 };
