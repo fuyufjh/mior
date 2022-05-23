86ed05a5875200277335d500c52f07e6dd6f2cf4
Eric Fu <fuyufjh@gmail.com>
2022-05-23 23:20:41 +0800

    fix support for CDATA

--- src/fetch.rs
+++ src/fetch.rs
@@ -25,8 +25,8 @@ fn parse_xml(text: impl BufRead) -> Result<FeedInfo> {
                     b"channel" => break parse_channel(&mut reader),
                     _ => (),
                 }
-            },
-            Ok(Event::Eof) => break Err(anyhow!("tag <channel> not found")),
+            }
+            Ok(Event::Eof) => break Err(anyhow!("Tag <channel> not found")),
             Err(e) => break Err(anyhow!("Error at position {}: {:?}", reader.buffer_position(), e)),
             _ => (),
         }
@@ -42,18 +42,16 @@ fn parse_channel(reader: &mut Reader<impl BufRead>) -> Result<FeedInfo> {
             Ok(Event::Start(ref e)) => {
                 match e.name() {
                     b"title" => {
-                        meta.title = reader.read_text(e.name(), &mut Vec::new())?;
+                        meta.title = parse_text(reader, e.name())?;
                     }
                     b"item" => {
                         items.push(parse_item(reader)?);
                     }
                     _ => (),
                 }
-            },
-            Ok(Event::End(ref e)) if e.name() == b"channel" => {
-                break Ok(FeedInfo{ meta, items })
             }
-            Ok(Event::Eof) => break Err(anyhow!("tag <channel> not closed")),
+            Ok(Event::End(ref e)) if e.name() == b"channel" => break Ok(FeedInfo { meta, items }),
+            Ok(Event::Eof) => break Err(anyhow!("Tag <channel> not closed")),
             Err(e) => break Err(anyhow!("Error at position {}: {:?}", reader.buffer_position(), e)),
             _ => (),
         }
@@ -68,26 +66,35 @@ fn parse_item(reader: &mut Reader<impl BufRead>) -> Result<FeedItem> {
             Ok(Event::Start(ref e)) => {
                 match e.name() {
                     b"title" => {
-                        feed_item.title = reader.read_text(e.name(), &mut Vec::new())?;
+                        feed_item.title = parse_text(reader, e.name())?;
                     }
                     b"link" => {
-                        feed_item.link = reader.read_text(e.name(), &mut Vec::new())?;
+                        feed_item.link = parse_text(reader, e.name())?;
                     }
                     _ => (),
                 }
-            },
-            Ok(Event::End(ref e)) if e.name() == b"item" => {
-                break Ok(feed_item)
             }
-            Ok(Event::Eof) => break Err(anyhow!("tag <item> not closed")),
+            Ok(Event::End(ref e)) if e.name() == b"item" => break Ok(feed_item),
+            Ok(Event::Eof) => break Err(anyhow!("Tag <item> not closed")),
             Err(e) => break Err(anyhow!("Error at position {}: {:?}", reader.buffer_position(), e)),
             _ => (),
         }
     }
 }
 
-
-///////////////////////////////////////////
+fn parse_text(reader: &mut Reader<impl BufRead>, tag: &[u8]) -> Result<String> {
+    let mut buf = Vec::new();
+    let s = match reader.read_event(&mut buf) {
+        Ok(Event::Text(e)) => e.unescape_and_decode(reader)?,
+        Ok(Event::CData(e)) => String::from_utf8(e.to_vec())?,
+        Ok(Event::End(ref e)) if e.name() == tag => return Ok("".to_string()),
+        Err(e) => return Err(anyhow!("Error at position {}: {:?}", reader.buffer_position(), e)),
+        Ok(Event::Eof) => return Err(anyhow!("Tag <{}> not closed", String::from_utf8_lossy(tag))),
+        _ => return Err(anyhow!("Text not found")),
+    };
+    reader.read_to_end(tag, &mut buf)?;
+    Ok(s)
+}
 
 #[cfg(test)]
 mod tests {
@@ -109,16 +116,18 @@ mod tests {
 
     test_case!("1", test_xml_1);
     test_case!("2", test_xml_2);
+    test_case!("3", test_xml_3);
 
     fn test_xml(name: &str) -> Result<()> {
         info!("running test {name}.xml...");
         let result = {
             let file = File::open(format!("{PATH}/{name}.xml"))?;
             let reader = BufReader::new(file);
-            let parsed = parse_xml(reader) ?;
-            serde_json::to_string_pretty( & parsed).unwrap()
+            let parsed = parse_xml(reader)?;
+            serde_json::to_string_pretty(&parsed).unwrap()
         };
 
+        // Uncomment following code to generate result files
         {
             let file = File::create(format!("{PATH}/{name}.result.json"))?;
             let mut writer = BufWriter::new(file);
